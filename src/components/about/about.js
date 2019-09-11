a708684e10d08f8ed2e656dcbbc26399d873f737
Jonathan Hsu <bit172@gmail.com>
2019-09-11 00:42:35 -0400

    Added Face Image, Fonts

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
-
+import avatar from "./avatar.png";
 const About = () => {
   return (
     <Container>
       <Row>
         <Col xs={12} md={6} className="text-right d-none d-lg-block">
-          <Image src="holder.js/250x400" rounded />
+          <Image src={avatar} roundedCircle height={350} width={350}/>
         </Col>
         <Col xs={12} md={6} className="text-center d-lg-none">
-          <Image src="holder.js/250x400" rounded />
+          <Image src={avatar} roundedCircle height={350} width={350}/>
         </Col>
         <Col xs={12} md={6}>
-          Jonathan Hsu
+          <h1>Jonathan Hsu</h1>
         </Col>
       </Row>
     </Container>
