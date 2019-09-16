be0fd984b0eeed6c636ac2b8c0b2decbb465cd2d
Jonathan Hsu <bit172@gmail.com>
2019-09-15 23:34:06 -0400

    Styling for Experience

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,31 +1,42 @@
 import React from "react";
-import { Container, Col } from "react-bootstrap";
-
+import { Container, Col, Image } from "react-bootstrap";
+import genetec from "./images/genetec.png";
 const Experience = () => {
   return (
-    <Container>
-      <div className="container-content">
-        <h1 className="section-title">Experience</h1>
-        <Col className="d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div className="resume-content">
-            <h3 className="mb-0">Electronic Production Tester Intern</h3>
-            <div className="subheading mb-3">Genetec</div>
-            <p className="experience-text">
-              Developed part of a database management web application to keep
-              track of data associated with parts in manufacturing for the
-              Electronic Production team. Created UI elements and configured
-              HTTPS requests to a back-end web server in Angular. Implemented
-              back end modules using a CQRS (Command Query Responsibility
-              Segregation) architechture in C#. Developped unit tests for
-              modules created.
-            </p>
-          </div>
-          <Col md={3} className="resume-date text-md-right">
-            <span>September 2018 - December 2018</span>
+    <div className="experience-section">
+      <Container>
+        <div className="container-content">
+          <h1 className="section-title">Experience</h1>
+          <Col className="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="experience-title mb-0">
+                Electronic Production Tester Intern
+              </h3>
+              <div className="company-title mb-3">
+                Genetec
+                <Image
+                  src={genetec}
+                  height={"25%"}
+                  width={"25%"}
+                  className="company-image"
+                />
+              </div>
+              <p className="experience-text">
+                Developed part of a internal web tool that keeps track of data
+                associated with manufactured parts for the Electronic Production
+                team. Created UI elements and configured HTTP requests to a
+                back-end web server in Angular. Implemented back end modules
+                using a CQRS (Command Query Responsibility Segregation)
+                architechture in C#. Developped unit tests for modules created.
+              </p>
+            </div>
+            <Col md={3} className="resume-date text-md-right">
+              <span>September 2018 - December 2018</span>
+            </Col>
           </Col>
-        </Col>
-      </div>
-    </Container>
+        </div>
+      </Container>
+    </div>
   );
 };
 
