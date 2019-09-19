f2139a2789e8297aee6a5e02eac341ba827e51a2
Jonathan Hsu <bit172@gmail.com>
2019-09-19 00:56:46 -0400

    Responsive Design, Skills with Circular Progress

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "./skills.css";
 const Skills = () => {
   return (
-    <Container>
-      <div className="container-content">
-        <h1 className="section-title">Skills</h1>
-      </div>
-    </Container>
+    <div className="experience-section">
+      <Container>
+        <div className="container-content">
+          <h1 className="section-title">Skills</h1>
+          <div className="skills-container">
+            <CircularProgressbar value={100} text={"100%"} strokeWidth={5} />
+            <CircularProgressbar value={90} text={"90%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+            <CircularProgressbar value={60} text={"60%"} strokeWidth={5} />
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
